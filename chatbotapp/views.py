ef8b6aaa5eb909a5a04460f361a8e305efbe1756
Wojciech Chodasiewicz <69510054+hcketjow@users.noreply.github.com>
2023-04-25 12:36:04 +0200

    changes

--- chatbotapp/views.py
+++ chatbotapp/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, HttpResponse
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 import json
-from chatterbot.trainers import ListTrainer
 from .models import Conversation
 import nltk
 import ssl
@@ -17,20 +16,30 @@ else:
 with open('data.json') as file:
     data = json.load(file)
 
+
 patterns = []
 responses = []
+
 for intent in data['intents']:
     for pattern in intent['patterns']:
         patterns.append(pattern)
     for response in intent['responses']:
         responses.append(response)
 
-bot = ChatBot('chatbot', read_only=False)
+bot = ChatBot('chatbot', read_only=False, logic_adapters=[
+    {
+        'import_path': 'chatterbot.logic.BestMatch',
+        'default_response': 'I am sorry, I do not understand what you are trying to say. Can you please rephrase your question or provide more information?',
+        'threshold': 0.7,
+        'maximum_similarity_threshold': 0.95,
+    },
+])
+
 trainer = ListTrainer(bot)
 trainer.train(patterns)
 trainer.train(responses)
 
-nltk.download()
+nltk.download('punkt')
 
 
 def main(request):
